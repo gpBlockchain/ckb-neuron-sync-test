1ef047b7105e03999bf3330fcec866db1247b9ea
xueyanli <xueyanli19950914@gmail.com>
2023-11-23 14:49:01 +0800

    add assert

--- src/tests/csv.test.ts
+++ src/tests/csv.test.ts
@@ -6,7 +6,7 @@ describe('csv', function () {
     it.skip("tocsv", async () => {
         await exportToCSVFiles({
             descPath: "tmp/fullNode",
-            sqlPath: "tmp/fullNode/wallet1/cell0-0x9c96d0b369b5fd42d7e6b30d6dfdb46e32dac7293bf84de9d1e2d11ca7930717.sqlite"
+            sqlPath: "tmp/fullNode/wallet1/cell-0x9c96d0b369b5fd42d7e6b30d6dfdb46e32dac7293bf84de9d1e2d11ca7930717.sqlite"
         })
     })
 
