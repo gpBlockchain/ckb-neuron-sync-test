3a553ce9d14257fbc27b5fe9006435b7374782ca
gpBlockchain <744158715@qq.com>
2023-12-06 17:06:08 +0800

    add linux

---
+++ prepare_neuron_linux.sh
@@ -0,0 +1,4 @@
+wget https://github.com/nervosnetwork/neuron/releases/download/v0.111.1/Neuron-v0.111.1-x86_64.AppImage
+./Neuron-v0.111.1-x86_64.AppImage --appimage-extract
+mkdir neuron
+mv squashfs-root neuron
