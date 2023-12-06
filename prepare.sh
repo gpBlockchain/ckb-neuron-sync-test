c0ed018bbd25ae1cba53329dee3fd1bd6c56e5e3
gpBlockchain <744158715@qq.com>
2023-12-06 16:57:48 +0800

    add prepare

--- prepare.sh
+++ prepare.sh
@@ -1,8 +1,3 @@
 tar -zxvf source.tar.gz
 bash script/download-ckb.sh $1
-git clone https://github.com/nervosnetwork/neuron.git
-cd neuron
-yarn global add lerna
-yarn
-lerna run rebuild:nativemodules
 echo "prepare successful"
