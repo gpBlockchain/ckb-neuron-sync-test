ffb8eeda56aacc76f6302790b3ee293e517f9e29
gpBlockchain <744158715@qq.com>
2023-12-06 23:18:16 +0800

    fix mac os is x64

--- prepare_neuron_macos.sh
+++ prepare_neuron_macos.sh
@@ -1,4 +1,4 @@
-wget https://github.com/nervosnetwork/neuron/releases/download/v0.111.1/Neuron-v0.111.1-mac-arm64.zip
-unzip Neuron-v0.111.1-mac-arm64.zip
+wget https://github.com/nervosnetwork/neuron/releases/download/v0.111.1/Neuron-v0.111.1-mac-x64.zip
+unzip Neuron-v0.111.1-mac-x64.zip
 mv Neuron.app neuron
 chmod 777 neuron/Contents
